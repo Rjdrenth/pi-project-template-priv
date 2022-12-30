6f566a56592f42d3b06115a8572b69b35a3a9841
R. Drenth <rj.drenth@gmail.com>
2022-12-30 16:04:19 +0100

    fix: actually use specified package version

--- {{cookiecutter.project_repository_name}}/tests/test_{{cookiecutter.python_package_name}}.py
+++ {{cookiecutter.project_repository_name}}/tests/test_{{cookiecutter.python_package_name}}.py
@@ -2,7 +2,7 @@ from {{cookiecutter.python_package_name}} import __version__
 
 
 def test_version():
-    assert __version__ == "0.1.0"
+    assert __version__ == "{{cookiecutter.package_version}}"
 
 
 if __name__ == "__main__":
--- {{cookiecutter.project_repository_name}}/{{cookiecutter.python_package_name}}/__init__.py
+++ {{cookiecutter.project_repository_name}}/{{cookiecutter.python_package_name}}/__init__.py
@@ -3,4 +3,4 @@ Root of the {{cookiecutter.python_package_name}} package.
 Add a high-level description here.
 """
 
-__version__ = "0.1.0"
+__version__ = "{{cookiecutter.package_version}}"
